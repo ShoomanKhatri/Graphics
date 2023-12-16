c9d96413d69d85fce104e26140735927bce9749d
Shoomankhatri <shoomankhatri@gmail.com>
2023-12-16 21:22:06 +0545

    solving errors

---
+++ Functions.cpp
@@ -0,0 +1,18 @@
+
+#include <graphics.h> 
+  
+int main() 
+{ 
+   
+    int gd = DETECT, gm; 
+    initgraph(&gd, &gm, ""); 
+  
+//   rectangle(125,115,215,165);  
+    circle(250, 200, 100); 
+    circle(400,200,50);
+    getch(); 
+
+    closegraph(); 
+  
+    return 0; 
+} 
