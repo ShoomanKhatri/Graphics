df6a1217fad15166bd8d739f8074aff22fa99b85
Shoomankhatri <shoomankhatri@gmail.com>
2024-02-06 12:28:06 +0545

    Translation added

---
+++ Translation.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <graphics.h> 
+#include<math.h>
+int main(){
+	int gd=DETECT, gm;
+	
+	initgraph(&gd,&gm, "C:\\DEVC\\MinGW64");
+	
+	int x1,x2, x3,x3;
+	
+	int tx, ty;
+	
+	
+	
+}
